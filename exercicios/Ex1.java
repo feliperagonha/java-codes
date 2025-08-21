14b5c7dc523c7d44ea0a7c25c3d264efe9faa1de
feliperagonha <felipe.lr3@puccampinas.edu.br>
2025-08-20 21:20:45 -0300

    Pasta Exercícios

---
+++ exercicios/Ex1.java
@@ -0,0 +1,19 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int soma = a + b;
+        int multiplicacao = a * b;
+        int divisao = a / b;
+        int subtracao = a - b;
+        int resto = a % b;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisao: " + divisao);
+        System.out.println("Subtracao: " + subtracao);
+        System.out.println("Resto: " + resto);
+
+    }
+}
